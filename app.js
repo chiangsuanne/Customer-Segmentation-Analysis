609034b82652a35b6b16b4922d459ad8d2159466
Suanne Chiang <108243961+chiangsuanne@users.noreply.github.com>
2023-11-14 18:20:24 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function initializeViz() {
   // JS object that points at empty div in the html
   var placeholderDiv = document.getElementById("tableauViz");
   // URL of the viz to be embedded
-  var url = "http://public.tableau.com/views/WorldIndicators/GDPpercapita";
+  var url = "https://public.tableau.com/views/CustomerSegmentationAnalysis_16963810307130/CustomerSegmentationAnalysis?:language=en-US&:display_count=n&:origin=viz_share_link";
   // An object that contains options specifying how to embed the viz
   var options = {
     width: '600px',
